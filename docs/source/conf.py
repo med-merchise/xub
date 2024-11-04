3c62408fe37b18b4f804478b9255a8fc11aab7af
Medardo Antonio Rodriguez <med.merchise@gmail.com>
2024-11-04 11:42:37 -0500

    Create initial documentation files

---
+++ docs/source/conf.py
@@ -0,0 +1,50 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'xub'
+copyright = '2025, Medardo Antonio Rodriguez'
+author = 'Medardo Antonio Rodriguez'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# -- MyST-Parser Extensions --------------------------------------------------
+# https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
+
+myst_enable_extensions = [
+    'attrs_block',
+    'attrs_inline',
+]
+
+myst_heading_anchors = 2  # Auto-generated header anchors
+
+# -- Configuration for `intersphinx` extension -------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html
+intersphinx_mapping = {
+    'py3': ('https://docs.python.org', None),
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,6 @@
+xub - Configuring Linux-based workstations
+==========================================
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents:
